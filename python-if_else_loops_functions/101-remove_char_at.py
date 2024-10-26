fffe5ae9ad0a190badef4c83c99d9f43cd38f2a7
Mukasa-E <e.simiyu@alustudent.com>
2024-10-26 16:00:08 -0700

    done

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-
 def remove_char_at(str, n):
-    """Remove character at the specified index n in the string."""
+    # If n is negative or out of range, return the original string
     if n < 0 or n >= len(str):
         return str
+    # Return the string excluding the character at index n
     return str[:n] + str[n + 1:]
-
