7a2069dc2115a0c7e194fc18eb7e3bc3a964100e
Mukasa-E <e.simiyu@alustudent.com>
2024-10-26 17:28:40 -0700

    done

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+def multiply_by_2(a_dictionary):
+    return {k: v * 2 for k, v in a_dictionary.items()}
