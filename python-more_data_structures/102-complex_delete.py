46e88e44498a866cee0a7a33c3708b87eeb26312
Mukasa-E <e.simiyu@alustudent.com>
2024-10-26 17:49:48 -0700

    done

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return {k: v for k, v in a_dictionary.items() if v != value}
+    # Create a list of keys to delete from the dictionary
+    keys_to_delete = [k for k, v in a_dictionary.items() if v == value]
+    
+    # Delete the keys from the original dictionary
+    for k in keys_to_delete:
+        del a_dictionary[k]
