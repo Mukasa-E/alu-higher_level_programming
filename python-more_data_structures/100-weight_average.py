b265c749f6f3e9b9c0c415c1ec8d7612eeaa5fbc
Mukasa-E <e.simiyu@alustudent.com>
2024-10-26 17:41:07 -0700

    done

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     if not my_list:  # Check if the list is empty
         return 0
-    total_score = sum(score * weight for score, weight in my_list)  # Calculate weighted scores
-    total_weight = sum(weight for _, weight in my_list)  # Calculate total weight
+
+    total_score = sum(score * weight for score, weight in my_list)
+    total_weight = sum(weight for _, weight in my_list)
+    
     return total_score / total_weight  # Return weighted average
