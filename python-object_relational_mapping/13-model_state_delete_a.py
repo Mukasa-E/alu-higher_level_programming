aabc9f5b23fc8e4bc41cd1a9d078eb949800683a
Mukasa-E <e.simiyu@alustudent.com>
2025-01-06 21:11:38 -0800

    edited

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
-"""
-Script that deletes all State objects with a name containing the letter 'a'
-from the database hbtn_0e_6_usa.
+"""script that deletes all State objects with a
+name containing the letter a from the database
+hbtn_0e_6_usa
 """
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
-    # Parse command-line arguments
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-
-    # Create a database engine
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            username, password, db_name
-        ),
-        pool_pre_ping=True
-    )
-
-    # Bind the engine to the metadata of the Base class
-    Base.metadata.create_all(engine)
-
-    # Create a configured "Session" class
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+    Base.metadata.create_all(engine)
 
-    # Query and delete all State objects with a name containing the letter 'a'
-    states_to_delete = session.query(State).filter(
-        State.name.like("%a%")
-    )
-    print(f"States to delete (debug): {[state.name for state in states_to_delete]}")  # Debugging
+    states = session.query(State).filter(State.name.contains('a')).all()
 
-    for state in states_to_delete:
+    for state in states:
         session.delete(state)
 
-    # Commit the changes to the database
     session.commit()
 
-    # Close the session
     session.close()
