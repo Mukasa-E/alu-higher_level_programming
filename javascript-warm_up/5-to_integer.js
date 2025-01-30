390d99c38f5149f900ed711ae6bc12d9de0df242
Mukasa-E <e.simiyu@alustudent.com>
2025-01-30 05:14:38 -0800

    Done

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,13 +1,12 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
-if (process.argv[2] === undefined);{
-	console.log("Not a number");
-}else {
-	const num = parseInt(process.argv[2], 10 );
-
-        if (isNaN(num){
-		console.log("Not a number");
-	}else {
-		console.log(`My number: ${num}`);
-	}
+if (process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  const num = parseInt(process.argv[2], 10);
+  if (isNaN(num)) {
+    console.log('Not a number');
+  } else {
+    console.log(`My number: ${num}`);
+  }
 }
