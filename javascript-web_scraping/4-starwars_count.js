4433ceee60eea2295090c45ea1b4f4f65e671943
Mukasa-E <e.simiyu@alustudent.com>
2025-02-14 11:17:31 -0800

    done

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,28 @@ const request = require('request');
 const url = process.argv[2];
 const characterId = 18;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const count = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    ).length;
-    console.log(count);
-  }
-});
+let count = 0;
+
+function fetchFilms (url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const data = JSON.parse(body);
+      data.results.forEach((film) => {
+        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+          count++;
+        }
+      });
+
+      // Check if there is a next page
+      if (data.next) {
+        fetchFilms(data.next); // Fetch the next page
+      } else {
+        console.log(count); // Print the final count
+      }
+    }
+  });
+}
+
+fetchFilms(url);
