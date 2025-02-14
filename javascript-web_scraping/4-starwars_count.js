6e2181ba820468a2c30fcd3a5ac8033ca51466b6
Mukasa-E <e.simiyu@alustudent.com>
2025-02-14 11:21:44 -0800

    done

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-const characterId = 18;
 
-let count = 0;
-
-function fetchFilms (url) {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      const data = JSON.parse(body);
-      data.results.forEach((film) => {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-          count++;
-        }
-      });
-
-      // Check if there is a next page
-      if (data.next) {
-        fetchFilms(data.next); // Fetch the next page
-      } else {
-        console.log(count); // Print the final count
-      }
-    }
-  });
-}
-
-fetchFilms(url);
+request(process.argv[2], function (err, res, body) {
+  if (!err) {
+    const results = JSON.parse(body).results;
+    console.log(
+      results.reduce((count, film) => {
+        return film.characters.find((character) => character.endsWith('/18/'))
+          ? count + 1
+          : count;
+      }, 0)
+    );
+  }
+});
